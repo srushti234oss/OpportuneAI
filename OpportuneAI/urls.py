dd59b049a287722358b86e0e59768b1db013e1ed
Sharanagouda Madabal <sharanagoudamadabal@Sharanagoudas-MacBook-Air.local>
2026-05-20 11:16:50 +0530

    Initial project upload

--- OpportuneAI/urls.py
+++ OpportuneAI/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.home, name='home'),
-    path('scholarships/', views.scholarships, name='scholarships'),
-    path('internships/', views.internships, name='internships'),
-    path('hackathons/', views.hackathons, name='hackathons'),
-    path('fellowships/', views.fellowships, name='fellowships'),
-    path('tracker/', views.tracker, name='tracker'),
-    path('skillmap/', views.skillmap, name='skillmap'),
+    path('', include('core.urls')),
 ]
---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('scholarships/', views.scholarships, name='scholarships'),
+    path('internships/', views.internships, name='internships'),
+    path('hackathons/', views.hackathons, name='hackathons'),
+    path('fellowships/', views.fellowships, name='fellowships'),
+    path('tracker/', views.tracker, name='tracker'),
+    path('skillmap/', views.skillmap, name='skillmap'),
+]
